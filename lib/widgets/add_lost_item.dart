2c1743ad2e0ee2e2a0ef15f6ba90596dc9846120
Vegros <mattias.tonna.g51962@mcast.edu.mt>
2025-03-14 21:41:37 +0100

    added add lost item page

---
+++ lib/widgets/add_lost_item.dart
@@ -0,0 +1,285 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lost_and_found/models/lost_Item.dart';
+
+class AddLostItem extends StatefulWidget {
+  const AddLostItem({super.key});
+
+  @override
+  State<AddLostItem> createState() {
+    return _LostItemsState();
+  }
+}
+
+class _LostItemsState extends State<AddLostItem> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredItemName = "";
+  var _enteredName = "";
+  var _enteredEmail = "";
+  var _enteredPhone = "";
+
+  File? _selectedImage;
+
+  void _saveItem() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      if (_selectedImage == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Please select an image!"),
+            backgroundColor: Colors.red,
+          ),
+        );
+        return;
+      }
+      String? base64Image;
+      try {
+        List<int> imageBytes = await _selectedImage!.readAsBytes();
+        base64Image = base64Encode(imageBytes);
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Failed to read image!"),
+            backgroundColor: Colors.red,
+          ),
+        );
+        return;
+      }
+      final url = Uri.https(
+          "lost-and-found-9b9cd-default-rtdb.europe-west1.firebasedatabase.app",
+          "lost_items.json");
+      final response = await http.post(
+        url,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json.encode({
+          'date': DateTime.now().toIso8601String(),
+          'itemName': _enteredItemName,
+          "itemImage": base64Image,
+          'contactName': _enteredName,
+          'contactEmail': _enteredEmail,
+          'contactPhone': _enteredPhone,
+        }),
+      );
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw Exception(
+            "Failed to save item. Status code: ${response.statusCode}");
+      }
+
+      Map<String, dynamic> response_data = json.decode(response.body);
+
+      if (!context.mounted) return;
+
+      var newLostItem = LostItem(
+        id: response_data['name'],
+        date: DateTime.now().toIso8601String(),
+        name: _enteredItemName,
+        image: base64Image,
+        contactName: _enteredName,
+        contactEmail: _enteredEmail,
+        contactPhone: _enteredPhone,
+      );
+      // Navigator.of(context).pop(newLostItem);
+    }
+  }
+
+  Future _pickImageFromGallery() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (returnedImage == null) return;
+
+    setState(() {
+      _selectedImage = File(returnedImage.path);
+    });
+  }
+
+  Future _pickImageFromCamera() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if (returnedImage == null) return;
+
+    setState(() {
+      _selectedImage = File(returnedImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lost & Found Home'),
+        centerTitle: true,
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Item Details',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      labelText: "Item Name",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          value.length > 50) {
+                        return "Incorrect Item Name!";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredItemName = value!;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: _pickImageFromGallery,
+                          child: const Text('From Gallery'),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: _pickImageFromCamera,
+                          child: const Text('From Camera'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Contact Me',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      labelText: "Name",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          value.length > 50) {
+                        return "Incorrect Name!";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredName = value!;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          !value.contains('@')) {
+                        return "Incorrect Email!";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredEmail = value!;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    maxLength: 8,
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                      labelText: "Mobile",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          value.length != 8) {
+                        return "Incorrect Phone!";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredPhone = value!;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          _saveItem();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                        textStyle: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      child: const Text('Mark Item as Lost'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+    // if (_selectedImage != null)
+    //             ClipRRect(
+    //               borderRadius: BorderRadius.circular(30),
+    //               child: Image.file(
+    //                 _selectedImage!,
+    //                 width: 200,
+    //                 height: 200,
+    //                 fit: BoxFit.cover,
+    //               ),
+    //             ),
